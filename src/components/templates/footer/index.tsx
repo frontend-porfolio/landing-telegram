d8fae7d456839fa373c82de89bcccd530288ed49
alel2003 <andrievskayalena22@gmail.com>
2025-07-01 12:48:05 +0500

    feat(src/components/templates/footer): added component for footer

--- src/components/templates/footer/index.tsx
+++ src/components/templates/footer/index.tsx
@@ -1,3 +1,8 @@
 export default function Footer() {
-  return <div></div>
+  return (
+    <footer className="mx-auto container border-t-2 border-gray-300 py-6 flex justify-between items-center">
+      <span className="text-black text-[17px] font-medium">©TYelegards 2022</span>
+    <span className="text-[#BBBBBB] text-[20px] font-bold uppercase">telegads</span>
+    </footer>
+  )
 }
