6376d575b12cff375fb65bfb7762769a0f56f444
alel2003 <andrievskayalena22@gmail.com>
2025-07-04 20:07:08 +0500

    feat(src/components/templates/navbar): improved code

--- src/components/templates/navbar/index.tsx
+++ src/components/templates/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import { useTranslation } from "react-i18next"
 
@@ -35,12 +35,23 @@ export default function Navbar() {
     }
   ] as const
 
+  useEffect(() => {
+        if (isMenu) {
+            document.body.classList.add("body--no-scroll")
+        } else {
+            document.body.classList.remove("body--no-scroll")
+        }
+
+        return () => {
+            document.body.classList.remove("body--no-scroll")
+        }
+    }, [isMenu])
+
   return (
     <nav className="relative z-100 container mx-auto flex items-center justify-between px-5 py-[30px]">
       <a className={`${isMenu ? 'hidden' : 'flex'} md:flex`} href="#">
         <img src="/icons/logo.svg" alt="logo" />
       </a>
-
        <div
         className={`${isMenu ? 'order-1' : 'order-0'} flex cursor-pointer md:hidden relative z-10`}
         onClick={() => setIsMenu(!isMenu)}>
@@ -50,16 +61,7 @@ export default function Navbar() {
           <CiMenuFries className="text-[30px] text-white" />
         )}
       </div>
-      <div className={`${isMenu ? 'flex' : 'hidden'} md:flex z-10`}>
-        <Language
-          lang={i18n.language}
-          changeLanguage={changeLanguage}
-          en={t("navbar.languages.en")}
-          ru={t("navbar.languages.ru")}
-          isMenu={isMenu}
-        />
-      </div>
-      <div className={`${isMenu ? 'flex' : 'hidden'} z-0 md:flex md:flex-row flex-col md:justify-start justify-center items-center gap-[50px] md:static absolute md:bg-transparent bg-white w-full h-screen top-0 bottom-0 left-0 right-0`}>
+      <div className={`${isMenu ? 'flex' : 'hidden'} z-0 md:flex md:flex-row flex-col md:justify-start justify-center items-center gap-[50px] md:static fixed md:bg-transparent bg-white md:w-fit w-full md:h-fit h-screen top-0 bottom-0 left-0 right-0`}>
         {links.map(({ to, label }) => (
           <a
             className="md:text-[17px] text-[20px] font-medium text-black transition-colors duration-300 ease-in-out hover:text-[#7044CC]"
@@ -68,6 +70,15 @@ export default function Navbar() {
             {label}
           </a>
         ))}
+      </div>
+       <div className={`${isMenu ? 'flex' : 'hidden md:flex'} z-10`}>
+        <Language
+          lang={i18n.language}
+          changeLanguage={changeLanguage}
+          en={t("navbar.languages.en")}
+          ru={t("navbar.languages.ru")}
+          isMenu={isMenu}
+        />
       </div>
     </nav>
   )
