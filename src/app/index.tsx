d5497717ee5cc1ee91b9a9925f78ff90488ff69e
alel2003 <andrievskayalena22@gmail.com>
2025-07-04 20:47:52 +0500

    feat(src/app): added new block animation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <div className="relative flex h-screen flex-col justify-between overflow-x-hidden">
       <motion.div
-        className="absolute md:-top-70 md:-right-50 -top-20 -right-25 md:size-[600px] size-[230px] rounded-full bg-gradient-to-tr from-[#7044CC] via-[#7044CC] to-white blur-xl"
+        className="absolute -top-20 -right-25 size-[230px] rounded-full bg-gradient-to-tr from-[#7044CC] via-[#7044CC] to-white blur-xl md:-top-70 md:-right-50 md:size-[600px]"
         animate={{
           scale: [1, 1.1, 1],
           opacity: [0.8, 1, 0.8]
@@ -28,7 +28,31 @@ export default function App() {
       <main className="relative z-90 flex-1">
         <Header />
         <Trust />
+        <motion.div
+          className="absolute top-150 -left-45 size-[230px] rounded-full bg-gradient-to-tr from-[#7044CC] via-[#7044CC] to-white blur-xl md:top-150 md:-left-50 md:size-[400px]"
+          animate={{
+            scale: [1, 1.1, 1],
+            opacity: [0.8, 1, 0.8]
+          }}
+          transition={{
+            duration: 6,
+            repeat: Infinity,
+            ease: "easeInOut"
+          }}
+        />
         <Work />
+        <motion.div
+          className="absolute top-400 -right-45 size-[230px] rounded-full bg-gradient-to-tr from-[#7044CC] via-[#7044CC] to-white blur-xl md:top-400 md:-right-50 md:size-[400px]"
+          animate={{
+            scale: [1, 1.1, 1],
+            opacity: [0.8, 1, 0.8]
+          }}
+          transition={{
+            duration: 6,
+            repeat: Infinity,
+            ease: "easeInOut"
+          }}
+        />
         <Registration />
         <Questions />
       </main>
